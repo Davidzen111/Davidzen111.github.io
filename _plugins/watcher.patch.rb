3429973582b4791c1fe6fe8bbb6034dc240aabba
Davidzen <davidzen662@gmail.com>
2025-06-09 16:46:35 +0800

    customizing my personal blog

--- _plugins/watcher.patch.rb
+++ _plugins/watcher.patch.rb
@@ -6,7 +6,7 @@ module Jekyll
     module Watcher
         extend self
 
-        alias_method :original_listen_ignore_paths, :original_listen_ignore_paths
+        alias_method :original_listen_ignore_paths, :listen_ignore_paths
 
         def listen_ignore_paths(options)
             original_listen_ignore_paths(options) + [%r!.*\.TMP!i]
